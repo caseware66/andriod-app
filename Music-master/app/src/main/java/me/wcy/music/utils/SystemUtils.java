fcea035a9cd4145ac0a7901145b548bd5872031b
chenxi <157500647@qq.com>
2017-04-27 19:45:13 +0800

    homework

---
+++ Music-master/app/src/main/java/me/wcy/music/utils/SystemUtils.java
@@ -0,0 +1,75 @@
+package me.wcy.music.utils;
+
+import android.app.ActivityManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v4.app.NotificationCompat;
+import android.text.format.DateUtils;
+
+import java.util.List;
+import java.util.Locale;
+
+import me.wcy.music.R;
+import me.wcy.music.activity.SplashActivity;
+import me.wcy.music.constants.Extras;
+import me.wcy.music.model.Music;
+
+/**
+ * Created by hzwangchenyan on 2016/3/22.
+ */
+public class SystemUtils {
+
+    /**
+     * 判断是否有Activity在运行
+     */
+    public static boolean isStackResumed(Context context) {
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> runningTaskInfos = manager.getRunningTasks(1);
+        ActivityManager.RunningTaskInfo runningTaskInfo = runningTaskInfos.get(0);
+        return runningTaskInfo.numActivities > 1;
+    }
+
+    /**
+     * 判断Service是否在运行
+     */
+    public static boolean isServiceRunning(Context context, Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Notification buildNotification(Context context, Music music) {
+        String title = music.getTitle();
+        String subtitle = FileUtils.getArtistAndAlbum(music.getArtist(), music.getAlbum());
+        Bitmap cover = CoverLoader.getInstance().loadThumbnail(music);
+        Intent intent = new Intent(context, SplashActivity.class);
+        intent.putExtra(Extras.EXTRA_NOTIFICATION, true);
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setContentIntent(pendingIntent)
+                .setContentTitle(title)
+                .setContentText(subtitle)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setLargeIcon(cover);
+        return builder.build();
+    }
+
+    public static String formatTime(String pattern, long milli) {
+        int m = (int) (milli / DateUtils.MINUTE_IN_MILLIS);
+        int s = (int) ((milli / DateUtils.SECOND_IN_MILLIS) % 60);
+        String mm = String.format(Locale.getDefault(), "%02d", m);
+        String ss = String.format(Locale.getDefault(), "%02d", s);
+        return pattern.replace("mm", mm).replace("ss", ss);
+    }
+}
